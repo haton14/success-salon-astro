1c78b705530206f185f78e6009448197d9e0ec1d
haton14 <iioka.takumi.quena@gmail.com>
2023-03-23 11:19:29 +0900

    サイトマップ追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,22 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  site: 'https://success-salon.haton14.com',
+  integrations: [sitemap({
+    customPages: [
+      'https://success-salon.haton14.com/',
+      'https://success-salon.haton14.com/access',
+      'https://success-salon.haton14.com/concept',
+      'https://success-salon.haton14.com/dressing',
+      'https://success-salon.haton14.com/eyelash',
+      'https://success-salon.haton14.com/head-spa',
+      'https://success-salon.haton14.com/price',
+      'https://success-salon.haton14.com/straight']
+  }),]
 });
